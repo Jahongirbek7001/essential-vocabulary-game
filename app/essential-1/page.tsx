6d753fd29e30da3373ff402249d7262b3a71214d
Jahongirxon <xannazarovjahongir@gmail.com>
2024-08-17 15:31:55 +0500

    second commit responsive fixed

--- app/essential-1/page.tsx
+++ app/essential-1/page.tsx
@@ -7,7 +7,7 @@ const EssentialFirst = () => {
 
     return (
         <>
-            <main className="text-white text-xl xl:text-2xl grid grid-cols-2 md:grid-cols-5 mx-auto p-24 gap-5">
+            <main className="text-white text-xl xl:text-2xl grid grid-cols-1 md:grid-cols-5 mx-auto p-24 gap-3">
                 {
                     data.map((element) => (
                         <Link className=" border-2 p-2 flex justify-center rounded-lg shadow-lg" key={element.unit_id} href={`/essential-${element.book_id}/unit/${element.unit_id}`}>
--- app/essential-1/unit/[id]/page.tsx
+++ app/essential-1/unit/[id]/page.tsx
@@ -25,7 +25,6 @@ export default function Essential1({ params }: Essential1Props) {
 
     return (
         <>
-            <div>{id}</div>
             <Content />
         </>
     );
